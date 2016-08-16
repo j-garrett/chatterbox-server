29d92d9a9bca4017d880446fcd1e3c62fba5eaef
jon-is-learning <jonathan.garrett@gmail.com>
2016-08-16 12:36:52 -0700

    Serve up index.html with fs module

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+// var index = './client/index.html';
 
 var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
@@ -6,51 +8,132 @@ var defaultCorsHeaders = {
   'access-control-max-age': 10 // Seconds.
 };
 
-
 var storage = {
   results: [{username: 'Dan', text: 'Banana!', objectId: 0 }, {username: 'jon', text: 'Gorilla!', objectId: 1 }]
 };
 var objectId = 1;
 
-var requestHandler = function(request, response) {  
+var requestHandler = function(req, res) {  
+  console.log(req.url);
 
-  var statusCode = 200;
-  var headers = defaultCorsHeaders;
-  headers['Content-Type'] = 'application/json';
+  if (req.url === '/') { //req.url has the pathname, check if it conatins '.html'
 
-  if (request.url !== '/classes/messages' && request.url !== '/classes/room') {
-    statusCode = 404;
-    response.writeHead(statusCode, headers);
-    response.end();
-  }
-
-  if (request.method === 'GET') {
-    response.writeHead(statusCode, headers);    
-    response.end(JSON.stringify(storage));
-  }
-
-  if (request.method === 'POST') {
-
-    var temp = '';
-    statusCode = 201;
-
-    request.on('data', function(chunk) {
-      temp += chunk;
-    });
-
-    request.on('end', function() {
-      var data = JSON.parse(temp);
-      data.objectId = ++objectId;
-      storage.results.push(data);
+    fs.readFile(__dirname + '/../client/index.html', function (err, data) {
+      if (err) { console.log(err); }
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
     });
 
   }
+
+  if (req.url.indexOf('.js') !== -1) { //req.url has the pathname, check if it conatins '.js'
+
+    fs.readFile(__dirname + '/../client/scripts/app.js', function (err, data) {
+      if (err) { console.log(err); }
+      res.writeHead(200, {'Content-Type': 'text/javascript'});
+      res.write(data);
+      res.end();
+    });
+
+  }
+
+  if (req.url.indexOf('.css') !== -1) { //req.url has the pathname, check if it conatins '.css'
+
+    fs.readFile(__dirname + '/../client/styles/styles.css', function (err, data) {
+      if (err) { console.log(err); }
+      res.writeHead(200, {'Content-Type': 'text/css'});
+      res.write(data);
+      res.end();
+    });
+  }
+
+  if (req.url.indexOf('.gif') !== -1) { //req.url has the pathname, check if it conatins '.css'
+
+    fs.readFile(__dirname + '/../client/images/spiffygif_46x46.gif', function (err, data) {
+      if (err) { console.log(err); }
+      res.writeHead(200, {'Content-Type': 'text/css'});
+      res.write(data);
+      res.end();
+    });
+  }
+
+  if (req.url === '/classes/messages' || req.url === '/classes/room' ) {
+    console.log('arrived at poop station');
+    var statusCode = 200;
+    var headers = defaultCorsHeaders;
+    headers['Content-Type'] = 'application/json';
+
+    if (req.method === 'GET') {
+      res.writeHead(statusCode, headers);    
+      res.end(JSON.stringify(storage));
+    }
+
+    if (req.method === 'POST') {
+
+      var temp = '';
+      statusCode = 201;
+
+      req.on('data', function(chunk) {
+        temp += chunk;
+      });
+
+      req.on('end', function() {
+        var data = JSON.parse(temp);
+        data.objectId = ++objectId;
+        storage.results.push(data);
+      });
+
+    }
+
+    headers['Content-Type'] = 'text/plain';
+
+    res.writeHead(statusCode, headers);
+
+    res.end(JSON.stringify(storage));
+  }
+
+  // if (req.url !== '/classes/messages' && req.url !== '/classes/room') {
+  //   statusCode = 404;
+  //   res.writeHead(statusCode, headers);
+  //   res.end();
+  // }
+
+  // if (req.method === 'GET') {
+  //   res.writeHead(statusCode, headers);    
+  //   res.end(JSON.stringify(storage));
+  // }
+
+  // if (req.method === 'POST') {
+
+  //   var temp = '';
+  //   statusCode = 201;
+
+  //   req.on('data', function(chunk) {
+  //     temp += chunk;
+  //   });
+
+  //   req.on('end', function() {
+  //     var data = JSON.parse(temp);
+  //     data.objectId = ++objectId;
+  //     storage.results.push(data);
+  //   });
+
+  // }
 
   // headers['Content-Type'] = 'text/plain';
 
-  response.writeHead(statusCode, headers);
+  // res.writeHead(statusCode, headers);
 
-  response.end(JSON.stringify(storage));
+  // res.end(JSON.stringify(storage));
+
 };
 
+
+
+
 exports.requestHandler = requestHandler;
+
+
+
+
